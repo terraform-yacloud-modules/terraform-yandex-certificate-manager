2ef68c790b3d6c0e218bff7050e8e92026a577c2
Anton Patsev <patsev.anton@gmail.com>
2024-07-30 07:58:00 +0600

    Немного поправил example

--- examples/self-managed/main.tf
+++ examples/self-managed/main.tf
@@ -1,3 +1,18 @@
+module "testsecret" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-lockbox.git"
+
+  name   = "testsecret"
+  labels = {}
+
+  entries = {
+    "key-a" : "value-a"
+    "key-pem" : file("key.pem")
+  }
+
+  deletion_protection = false
+}
+
+
 module "self_managed" {
   source = "../.."
 
@@ -11,8 +26,8 @@ module "self_managed" {
       description = "self-managed domain certificate from lockbox"
       certificate = "-----BEGIN CERTIFICATE----- ... -----END CERTIFICATE----- \n -----BEGIN CERTIFICATE----- ... -----END CERTIFICATE-----"
       private_key_lockbox_secret = {
-        id  = "lockbox_id"
-        key = "lockbox_key"
+        id  = module.testsecret.id
+        key = "key-pem"
       }
     }
   }
