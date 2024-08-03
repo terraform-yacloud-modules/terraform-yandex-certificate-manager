37629f8c37a3a6e61678c2cf78c4dbb3ecefe1ff
Anton Patsev <patsev.anton@gmail.com>
2024-08-03 19:12:24 +0600

    Добавил пример managed

--- examples/managed/main.tf
+++ examples/managed/main.tf
@@ -4,7 +4,7 @@ module "network" {
   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git?ref=v1.0.0"
 
   folder_id  = data.yandex_client_config.client.folder_id
-  blank_name = "instance-minimal-vpc-nat-gateway"
+  blank_name = "certificate-manager-dns"
 
   azs = ["ru-central1-a"]
 
@@ -13,43 +13,27 @@ module "network" {
   create_nat_gateway = true
 }
 
-module "yandex_compute_instance" {
-  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-instance.git?ref=v1.0.0"
-
-  folder_id = data.yandex_client_config.client.folder_id
-
-  name = "minimal-instance"
-
-  zone       = "ru-central1-a"
-  subnet_id  = module.network.private_subnets_ids[0]
-  enable_nat = true
-  create_pip = true
-
-  hostname         = "minimal-instance"
-  generate_ssh_key = false
-  ssh_user         = "ubuntu"
-  ssh_pubkey       = "~/.ssh/id_rsa.pub"
-
-  user_data = <<-EOF
-        #cloud-config
-        package_upgrade: true
-        packages:
-          - nginx
-        runcmd:
-          - [systemctl, start, nginx]
-          - [systemctl, enable, nginx]
-        EOF
-}
-
 module "dns_zone" {
 
   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-dns.git//modules/zone?ref=v1.0.0"
 
-  name        = "my-private-zone"
-
+  name             = "my-private-zone"
   zone             = "dns-zone.org.ru."
   is_public        = true
   private_networks = [module.network.vpc_id] # можете заменить на ваш network_id
+}
+
+module "address" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-address.git"
+
+  name    = "test"
+  zone_id = "ru-central1-a"
+  dns_record = {
+    fqdn        = "test"
+    dns_zone_id = module.dns_zone.id
+    ttl         = 300
+    ptr         = true
+  }
 }
 
 module "dns_recordset" {
@@ -59,19 +43,18 @@ module "dns_recordset" {
   name    = "test.dns-zone.org.ru."
   type    = "A"
   ttl     = 200
-  data    = [module.yandex_compute_instance.instance_public_ip]
+  data    = [module.address.external_ipv4_address]
 }
 
-# module "dns_letsencrypt" {
-#   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-dns.git//modules/recordset?ref=v1.0.0"
-#
-#   zone_id = module.dns_zone.id
-#   name    = module.managed.managed_certificates["test"].challenges[0].dns_name
-#   type    = module.managed.managed_certificates["test"].challenges[0].dns_value
-#   ttl     = 200
-#   data    = [module.managed.managed_certificates["test"].challenges[0].dns_value]
-# }
+module "dns_letsencrypt" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-dns.git//modules/recordset?ref=v1.0.0"
 
+  zone_id = module.dns_zone.id
+  name    = module.managed.managed_certificates["test"].challenges[0].dns_name
+  type    = "CNAME"
+  ttl     = 200
+  data    = [module.managed.managed_certificates["test"].challenges[0].dns_value]
+}
 
 module "managed" {
   source = "../../"
--- examples/managed/outputs.tf
+++ examples/managed/outputs.tf
@@ -1,11 +1,3 @@
-output "instance_private_ip" {
-  value = module.yandex_compute_instance.instance_private_ip
-}
-
-output "instance_public_ip" {
-  value = module.yandex_compute_instance.instance_public_ip
-}
-
 output "zone_id" {
   description = "The unique identifier of the DNS zone created by the module."
   value       = module.dns_zone.id
@@ -61,8 +53,7 @@ output "type" {
   value       = module.managed.managed_certificates["test"].challenges[0].type
 }
 
-output "updated_at" {
-  description = ""
-  value       = module.managed.managed_certificates["test"].challenges[0].updated_at
+output "external_ipv4" {
+  description = "External IPv4 address details"
+  value       = module.address.external_ipv4_address
 }
-
